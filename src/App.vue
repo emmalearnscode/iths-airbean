58d83863fbf9aa42d0ff6cb16bbd555e73c5ed94
Emma Dawson <emma.l.dawson@gmail.com>
2021-02-16 14:50:07 +0100

    added script tag

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  
 }
+</script>
+
+<style lang="scss">
+
 
 
 
