dfbb26fee001678992b28f515d0f97565076ff9a
Emma Dawson <emma.l.dawson@gmail.com>
2021-02-18 11:39:54 +0100

    Finish menu page with styling and modal

---
+++ src/components/ProductInfoModal.vue
@@ -0,0 +1,64 @@
+<template>
+
+<section>
+  <span class="close" @click="closeModal">x</span>
+  <h3>{{currentProd.title}}</h3>
+<p>{{currentProd.extraInfo}}</p>
+</section>
+
+</template>
+
+<script>
+export default {
+props: {
+  currentProd: {
+    type: Object
+  }
+},
+methods: {
+  closeModal() {
+    this.$emit("closeTheModal")
+  }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+$brown: #2F2926;
+section {
+  width: 90%;
+  min-height: 35rem;
+  background-color: white;
+  border-radius: 5px;
+  z-index: 1;
+position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0.5px 0.5px 10px $brown;
+ 
+  
+  padding: 1.5rem;
+
+  h3 {
+    margin-bottom: 1rem;
+    text-align: center;
+  }
+
+  p {
+    line-height: 1.6;
+  }
+
+  .close {
+    background-color: $brown;
+    padding: 1rem;
+    color: white;
+    text-transform: uppercase;
+    border-radius: 50%;
+    position: absolute;
+    top: -10px;
+    right: -10px;
+    font-size: 1.2rem;
+  }
+}
+</style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,22 +1,37 @@
 <template>
-  <div class="home">
+  <div class="menu-wrapper">
     <h1>Menu</h1>
     <ul>
       <li v-for="product in getProducts" :key="product.id">
-        <button @click="addToCart(product)">Add</button>
+        <button @click="addToCart(product)"><img src="@/assets/add.svg" alt="plus sign"></button>
+        <div @click="showExtraInfo(product)">
         <h3>{{product.title}}</h3>
-        <h3>{{product.price}}</h3>
         <p>{{product.desc}}</p>
+        </div>
+        <h3 class="price">{{product.price}} kr</h3>
+        
         
         
       </li>
     </ul>
+    <transition name="fade">  
+    <product-info-modal @closeTheModal="closeModal" v-if="infoModalIsOpen" :currentProd="clickedOnProduct"></product-info-modal>
+    </transition>  
   </div>
 </template>
 
 <script>
+
+import ProductInfoModal from '../components/ProductInfoModal.vue'
 export default {
   name: 'Menu',
+  components: {ProductInfoModal},
+  data() {
+    return {
+      infoModalIsOpen: false,
+      clickedOnProduct: null
+    }
+  },
   computed: {
     getProducts() {
       return this.$store.getters.getProducts
@@ -25,7 +40,64 @@ export default {
   methods: {
     addToCart(prod) {
       this.$store.dispatch("addToCart", prod)
+    },
+    showExtraInfo(prod) {
+    this.infoModalIsOpen = true;
+    this.clickedOnProduct = prod
+  },
+  closeModal() {
+    this.infoModalIsOpen = false;
+  }
+
+  },
+  
+}
+</script>
+
+<style lang="scss" scoped>
+$brown: #2F2926;
+.menu-wrapper {
+ 
+  h1 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  button {
+    width: 2.5rem;
+    height: 2.5rem;
+    border: none;
+    border-radius: 50%;
+    padding: 0.5rem;
+    background-color: $brown;
+    align-self: center;
+  }
+
+  ul {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 2.5rem;
+
+    p {
+      margin-top: 0.3rem;
     }
   }
 }
-</script>
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to, .fade-leave {
+  opacity: 1;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: all 0.5s ease;
+}
+</style>
